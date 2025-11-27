00f79316e469cb8c61094598f6c165081cda7db1
aksworns22 <wkdeogks17@gmail.com>
2025-11-27 15:11:17 +0900

    refactor(note): remove \\0 magic value

--- app/src/main/java/com/example/commitnote/core/Note.kt
+++ app/src/main/java/com/example/commitnote/core/Note.kt
@@ -1,5 +1,8 @@
 package com.example.commitnote.core
 
 data class Note(val title: String, val content: String) {
-    override fun toString(): String = "$title\\0$content"
+    override fun toString(): String = "$title$CONTENT_DELIMITER$content"
+    companion object {
+        const val CONTENT_DELIMITER = "\\0"
+    }
 }
--- app/src/test/java/com/example/commitnote/NoteTest.kt
+++ app/src/test/java/com/example/commitnote/NoteTest.kt
@@ -9,7 +9,7 @@ class NoteTest {
     @Test
     fun `제목과 내용이 포함된 노트를 구분자로 연결된 단일 문자열로 만든다`() {
         assertThat(Note(title = "hello", content = "world").toString())
-            .isEqualTo("hello\\0world")
+            .isEqualTo("hello${Note.CONTENT_DELIMITER}world")
     }
 
     @Test
