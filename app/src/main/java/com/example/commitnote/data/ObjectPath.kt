8c28f9b54f2a204862424ae7e106dd32cbd7236a
aksworns22 <wkdeogks17@gmail.com>
2025-11-27 20:35:14 +0900

    refactor(ObjectPath): rename from NotePath

--- app/src/main/java/com/example/commitnote/data/ObjectPath.kt
+++ app/src/main/java/com/example/commitnote/data/ObjectPath.kt
@@ -1,15 +1,21 @@
 package com.example.commitnote.data
 
 import com.example.commitnote.core.Hasher
+import com.example.commitnote.core.Commit
 import com.example.commitnote.core.Note
 
-class NotePath(val directory: String, val filename: String) {
+class ObjectPath(val directory: String, val filename: String) {
     val fullHash = directory + filename
     override fun toString(): String = "$directory/$filename"
     companion object {
-        fun of(note: Note, hasher: Hasher): NotePath {
+        fun of(note: Note, hasher: Hasher): ObjectPath {
             val fullPath = hasher.hash(note.toString())
-            return NotePath(getDirectoryPath(fullPath), getFilePath(fullPath))
+            return ObjectPath(getDirectoryPath(fullPath), getFilePath(fullPath))
+        }
+
+        fun of(commit: Commit, hasher: Hasher): ObjectPath {
+            val fullPath = hasher.hash(commit.toString())
+            return ObjectPath(getDirectoryPath(fullPath), getFilePath(fullPath))
         }
 
         private fun getDirectoryPath(fullPath: String) = fullPath.take(2)
