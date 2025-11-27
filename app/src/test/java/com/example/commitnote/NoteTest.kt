747ca694595cec6377c22d02d4ec11348daf4be1
aksworns22 <wkdeogks17@gmail.com>
2025-11-27 15:08:47 +0900

    test(note): verify sha1 hashing from note

--- app/src/test/java/com/example/commitnote/NoteTest.kt
+++ app/src/test/java/com/example/commitnote/NoteTest.kt
@@ -1,6 +1,7 @@
 package com.example.commitnote
 
 import com.example.commitnote.core.Note
+import com.example.commitnote.core.Sha1Hasher
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
@@ -9,5 +10,11 @@ class NoteTest {
     fun `제목과 내용이 포함된 노트를 구분자로 연결된 단일 문자열로 만든다`() {
         assertThat(Note(title = "hello", content = "world").toString())
             .isEqualTo("hello\\0world")
+    }
+
+    @Test
+    fun `노트에 대해 SHA-1 해싱을 적용한다`() {
+        assertThat(Sha1Hasher.hash(Note(title="hello", content = "world").toString()))
+            .isEqualTo("63b77d3e36388e6c463f85726a4bb4585479d96e")
     }
 }
