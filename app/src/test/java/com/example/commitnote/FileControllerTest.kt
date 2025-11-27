b183ed6dfbdc80338119cfb4113be0c07e9e7dc3
aksworns22 <wkdeogks17@gmail.com>
2025-11-27 16:48:39 +0900

    test(fileController): verify storing note data

---
+++ app/src/test/java/com/example/commitnote/FileControllerTest.kt
@@ -0,0 +1,27 @@
+package com.example.commitnote
+
+import android.content.Context
+import com.example.commitnote.core.Note
+import com.example.commitnote.data.FileController
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+
+class FileControllerTest {
+
+    @get:Rule
+    val tempFolder = TemporaryFolder()
+
+    @Test
+    fun verifyNoteFileIsCreated() {
+        val mockContext = Mockito.mock(Context::class.java)
+        `when`(mockContext.filesDir).thenReturn(tempFolder.root)
+        val note = Note(title = "hello", content = "world")
+        val file = FileController.write(mockContext.filesDir, note)
+        assertThat(file.path).contains("63/b77d3e36388e6c463f85726a4bb4585479d96e")
+        assertThat(file.readText(Charsets.UTF_8)).isEqualTo("hello\\0world")
+    }
+}
